17ec28b750f2c19745ca29bbcecc58b6f88b9acb
Richard A. Burgos <richyburgos@live.com>
2023-05-27 00:22:51 -0600

    Amended Footer

--- src/components/layout-layers/PortfolioFooter.vue
+++ src/components/layout-layers/PortfolioFooter.vue
@@ -1,9 +1,6 @@
 <template>
   <footer class="footer text-center py-2 theme-bg-dark">
-
-    <!--/* This template is free as long as you keep the footer attribution link. If you'd like to use the template without the attribution link, you can buy the commercial license via our website: themes.3rdwavemedia.com Thank you for your support. :) */-->
-    <small class="copyright">Designed with <span class="sr-only">love</span><i class="fas fa-heart" style="color: #fb866a;"></i> by <a href="#" target="_blank">Xiaoying Riley</a> for developers</small>
-
+    <small class="copyright">Created using Vue 3, Nuxt 3 and Bootstrap 5 by Richard Burgos</small>
   </footer>
 </template>
 
