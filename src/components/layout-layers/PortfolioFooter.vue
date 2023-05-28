dcac812ac7f287af11ed2ae4899162e05a2867a1
Richard A. Burgos <richyburgos@live.com>
2023-05-28 03:13:39 -0600

    Updated footer to add github project link

--- src/components/layout-layers/PortfolioFooter.vue
+++ src/components/layout-layers/PortfolioFooter.vue
@@ -1,11 +1,26 @@
 <template>
   <footer class="footer text-center py-2 theme-bg-dark">
-    <small class="copyright">Created using Vue 3, Nuxt 3 and Bootstrap 5 by Richard Burgos</small>
+    <small class="copyright">
+      Created using Vue 3, Nuxt 3 and Bootstrap 5 by Richard Burgos
+      <i>
+        <a href="https://github.com/richyburgos/portfolio">
+          <font-awesome-icon
+              :icon="['fab', 'github']"
+              size="xl"
+              fixed-width
+          />
+          See the project on Github
+        </a>
+      </i>
+    </small>
   </footer>
 </template>
 
 <script>
+import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
+
 export default {
+  components: {FontAwesomeIcon}
 }
 </script>
 
